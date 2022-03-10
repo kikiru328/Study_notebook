986c94f6e351f785442a008f00e28f856753c0d3
Daniel_Kim <60537388+kikiru328@users.noreply.github.com>
2022-03-10 17:00:39 +0900

    module import..
    module import..

--- scrapy/movie/movie/spiders/movieSpider.py
+++ scrapy/movie/movie/spiders/movieSpider.py
@@ -3,20 +3,25 @@ import scrapy
 # Class
 class MovieSpider(scrapy.Spider):
     # Spider name
-    name = 'Movie'
+    name = 'movie'
     
     def start_requests(self):
-        urls = ['https://movie.naver.com/movie/running/current.nhn']
-        
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        # urls = ['https://movie.naver.com/movie/running/current.nhn'] 
+        # for url in self.urls:
+        yield scrapy.Request(url=self.url, callback=self.parse)
     
     def parse(self, response):
         # response.css() vs response.xpath()
         movie_list = response.css('ul.lst_detail_t1 > li')
         for movie in movie_list:
-            title = movie.css('.tit > a::text').get()
+            # title = movie.css('.tit > a::text').get()
+            title = movie.css(self.title_css).get()
+            rate = movie.css('.num::text').get()
+            director = movie.css('.info_txt1 > dd > span.link_txt > a::text').get()
             print('title', title)
+            print('rate', rate)
+            print('director', director)
+            print('director', director)
         
         
-    
+    # content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(4) > dl > dd:nth-child(3) > dl > dd:nth-child(4) > span > a
