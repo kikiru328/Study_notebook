59a6af3e36235e6bfe758da6980ae4a8deef4b7f
Daniel_Kim <60537388+kikiru328@users.noreply.github.com>
2022-03-14 17:09:14 +0900

    middleware_selenium 연동
    middleware_selenium 연동

---
+++ scrapy/test_module/test_module/items.py
@@ -0,0 +1,17 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
+
+class TestModuleItem(scrapy.Item):
+    관객수 = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+   
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    # pass
