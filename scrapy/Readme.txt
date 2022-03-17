3ec5b649025c21cb997c006add6c55e97e8e65ca
Daniel_Kim <60537388+kikiru328@users.noreply.github.com>
2022-03-17 12:49:39 +0900

    scrapy

--- scrapy/Readme.txt
+++
@@ -1,8 +0,0 @@
-Scrapy spider 만들 때
-* cmd (terminal) 
-scrapy startproject {name}
-
-이후 cd ~~name
-
-spider 생성
-scrapy genspider {name} {url}
--- scrapy/movie/movie/PASS/.ipynb_checkpoints/A-checkpoint.py
+++
@@ -1,6 +0,0 @@
-def urls():
-    urls = ['https://movie.naver.com/movie/bi/mi/basic.naver?code=190991',
-     'https://movie.naver.com/movie/bi/mi/basic.naver?code=154282']
-    return urls
-
-    
